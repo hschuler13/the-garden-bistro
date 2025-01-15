e9fdc6699cb33895f77ec6cd1454d836ccdc3787
hschuler13 <hschul04@gmail.com>
2025-01-15 10:04:20 -0600

    breadcrumb fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,7 @@
-import { AppSidebar } from "@/components/app-sidebar"
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
-import Image from 'next/image'
-
 export default function Page() {
   return (
     <>
-      <BreadcrumbPage>Data Fetching</BreadcrumbPage>
       <img src="/theGardenBistroLogo.svg" ></img>
     </>
-      
   )
 }
